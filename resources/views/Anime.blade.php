8fcabe026592057681c21baa797c14074ba01f3b
czyner <czyner7@gmail.com>
2024-02-21 12:38:16 +0800

    Final Output

--- resources/views/Anime.blade.php
+++ resources/views/Anime.blade.php
@@ -1,3 +1,15 @@
 <div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+   </head>
+   <body>
+    <h1>Anime</h1>
+    @dd($data);
+   </body>
+   </html>
 </div>
